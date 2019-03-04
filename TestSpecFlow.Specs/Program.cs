20427ae6a50a050f0ef3948987b63d9725a06dc0
Mathieu DECROOCQ <mathieu.decroocq@econocom.com>
2019-03-04 13:12:29 +0100

    Ajoutez des fichiers projet.

---
+++ TestSpecFlow.Specs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSpecFlow.Specs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
