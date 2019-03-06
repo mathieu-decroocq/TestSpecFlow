9f061ff942b9e7ab3ce837b3dde40f3ff1fdd7f3
Mathieu DECROOCQ <mathieu.decroocq@econocom.com>
2019-03-06 13:36:41 +0100

    add step definitions and implementation

---
+++ TestSpecFlow.Specs/StepDefinitions/CalculatorSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace TestSpecFlow.Specs.StepDefinitions
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private Calculator calculator;
+
+        public CalculatorSteps()
+        {
+            calculator = new Calculator();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            calculator.Input1 = p0;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            calculator.Input2 = p0;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            calculator.Sum();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.Equal(p0, calculator.Result);
+        }
+    }
+}
---
+++ TestSpecFlow/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestSpecFlow
+{
+    public class Calculator
+    {
+        public int Input1 { get; set; }
+        public int Input2 { get; set; }
+        public int Result { get; set; }
+
+        public void Sum()
+        {
+            Result = Input1 + Input2;
+        }
+    }
+}
